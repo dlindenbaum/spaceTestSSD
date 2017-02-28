40da6dafb722a66db9e1227a30673feacdc0015e
dlindenbaum <dlindenbaum@iqt.org>
2017-02-27 20:52:19 -0500

    updated docker file_cuda8.0

--- ssd-spacenet-ssd/spacenet/utilities/python/gen-pascalvoc-format.py
+++ ssd-spacenet-ssd/spacenet/utilities/python/gen-pascalvoc-format.py
@@ -19,6 +19,74 @@ import cv2
 json_file = '/home/tempuser/spacenet-data/vectorData/summaryData/AOI_1_Rio_polygons_solution_3Band.geojson'
 folder_name = 'spacenet'
 
+
+
+def geoJsonWKTToPascalVOC(geoJson, rasterImage, img_id=''):
+
+
+
+    f = open(out_dir + '/' + im_id + '.xml', 'w')
+
+    root = ET.Element('annotation')
+    tree = ET.ElementTree(root)
+    folder_e = ET.SubElement(root, 'folder')
+    folder_e.text = folder_name
+    f_name_e = ET.SubElement(root, 'filename')
+    f_name_e.text = im_id + '.tif'
+
+    # set_trace() # define Image Size
+    im = cv2.imread('/home/tempuser/spacenet-data/3band' + '/' + '3band_' + f_name_e.text)
+    height, width, depth = im.shape  # (width,height) tuple
+    size_e = ET.SubElement(root, 'size')
+    depth_e = ET.SubElement(size_e, 'depth')
+    depth_e.text = '3'
+    height_e = ET.SubElement(size_e, 'height')
+    height_e.text = str(height)
+    width_e = ET.SubElement(size_e, 'width')
+    width_e.text = str(width)
+
+
+    ## write points
+    for poly in polyList:
+        obj_e = ET.SubElement(root, 'object')
+        name_e = ET.SubElement(obj_e, 'name')
+        name_e.text = 'building'
+        bndbox_e = ET.SubElement(obj_e, 'bndbox')
+
+        poly_pts = []
+        for p in range(pts.GetPointCount()):
+            poly_pts.append([int(round(pts.GetX(p))), int(round(pts.GetY(p)))])
+            print 'x {}, y {}'.format(poly_pts[-1][0], poly_pts[-1][1])
+            # f.write('x {}, y {}\n'.format(poly_pts[-1][0], poly_pts[-1][1]))
+
+        poly_pts = np.array(poly_pts[0:-1], np.int32)
+        poly_pts = poly_pts.reshape((-1, 1, 2))
+
+        ok, mnx_mny_mxx_mxy, width, height, area = bounding_rect_min_max(poly_pts)
+        (mnx, mny, mxx, mxy) = mnx_mny_mxx_mxy
+
+        xmin_e = ET.SubElement(bndbox_e, 'xmin')
+        xmin_e.text = str(int(mnx))
+        ymin_e = ET.SubElement(bndbox_e, 'ymin')
+        ymin_e.text = str(int(mny))
+        xmax_e = ET.SubElement(bndbox_e, 'xmax')
+        xmax_e.text = str(int(mxx))
+        ymax_e = ET.SubElement(bndbox_e, 'ymax')
+        ymax_e.text = str(int(mxy))
+
+        print 'box params w:{}, h:{}, area:{}\n'.format(width, height, area)
+        # f.write('box params w:{}, h:{}, area:{}\n'.format(width, height, area))
+
+    indent(root)
+    tree.write(f)
+    f.close()
+
+
+
+
+
+
+
 def bounding_rect(poly):
     n = np.shape(poly)[0]
     poly = poly.reshape(n,2)
@@ -45,6 +113,9 @@ def bounding_rect_min_max(poly):
         return True,  mn_mx_xy, w, h, w * h 
     else:
         return False, (0, 0, 0, 0), 0, 0, 0
+
+
+
 
 if __name__ == "__main__":
     gt_truth = json_file
--- ssd-spacenet-ssd/spacenet/utilities/python/gen-pascalvoc-format_v1.py
+++ ssd-spacenet-ssd/spacenet/utilities/python/gen-pascalvoc-format_v1.py
@@ -7,7 +7,7 @@ These files were created by cutting the the srcData above into 200m x 200m image
 
 from spaceNet import geoTools as gT
 import numpy as np
-from pudb import set_trace
+import csv
 from shapely.geometry.polygon import LinearRing
 import os
 import shutil
@@ -47,97 +47,94 @@ def bounding_rect_min_max(poly):
         return False, (0, 0, 0, 0), 0, 0, 0
 
 if __name__ == "__main__":
-    gt_truth = json_file
 
-    polys_sol = gT.importgeojson(gt_truth, removeNoBuildings=True)
+    summaryCSVFile = "AOI_1_Rio_polygons_solution_3Band.csv"
 
-    polys_sol_mids_list = np.asarray([item['ImageId'] 
-                                     for item in polys_sol if item["ImageId"] > 0 
-                                     and item['BuildingId']!=-1])
+    out_dir = "Annotations"
+    out_dir = '/home/tempuser/spacenet-data/Annotations'
+    img_dir = '/3band/'
+    img_prefix = '3band'
+    if os.path.exists(out_dir):
+        shutil.rmtree(out_dir)
+    os.makedirs(out_dir)
 
-    polys_sol_bids_list = np.asarray([item['BuildingId'] 
-                                     for item in polys_sol if item["ImageId"] > 0 
-                                     and item['BuildingId']!=-1])
-
-    polys_sol_polys_list = np.asarray([item['poly'] 
-                                      for item in polys_sol if item["ImageId"] > 0 
-                                      and item['BuildingId']!=-1])
-
-out_dir = '/home/tempuser/spacenet-data/Annotations'
-if os.path.exists(out_dir):
-    shutil.rmtree(out_dir)
-os.makedirs(out_dir)
-
-curr_im_id = ''
-f = open(out_dir + '/' + 'empty.xml', 'w')
-root = ET.Element('annotation')
-tree = ET.ElementTree(root)
-for im_id, b_id, geom in zip(polys_sol_mids_list, polys_sol_bids_list, 
-                          polys_sol_polys_list):
-    if curr_im_id != im_id:
-        indent(root)
-        #tree.write(sys.stdout)
-        tree.write(f)
-        f.close()
-        curr_im_id = im_id
-        f = open(out_dir + '/' + im_id + '.xml', 'w')
-
+    with open(summaryCSVFile, 'rb') as csvfile:
+        csvReader = csv.reader(csvfile, delimiter=',')
+        curr_im_id = ''
+        f = open(out_dir + '/' + 'empty.xml', 'w')
         root = ET.Element('annotation')
         tree = ET.ElementTree(root)
-        folder_e = ET.SubElement(root, 'folder')
-        folder_e.text = folder_name
-        f_name_e = ET.SubElement(root, 'filename')
-        f_name_e.text = im_id + '.tif'
 
-        # set_trace()
-        im = cv2.imread('/home/tempuser/spacenet-data/3band' + '/' + '3band_' + f_name_e.text)
-        height, width, depth = im.shape # (width,height) tuple
-        size_e = ET.SubElement(root, 'size')
-        depth_e = ET.SubElement(size_e, 'depth')
-        depth_e.text = '3'
-        height_e = ET.SubElement(size_e, 'height')
-        height_e.text = str(height)
-        width_e = ET.SubElement(size_e, 'width')
-        width_e.text = str(width)
+        for row in csvReader:
+            im_id = row[0]
+            b_id  = row[1]
+            polyPixWKT = row[2]
+            polyGeoWKT = row[3]
 
+            if curr_im_id != im_id:
+                indent(root)
+                #tree.write(sys.stdout)
+                tree.write(f)
+                f.close()
+                curr_im_id = im_id
+                f = open(out_dir + '/' + im_id + '.xml', 'w')
 
-    pts = geom.GetGeometryRef(0)
-    print '--------{}----{}------\n'.format(im_id, b_id)
-    # f.write('--------{}----{}------\n'.format(im_id, b_id))
+                root = ET.Element('annotation')
+                tree = ET.ElementTree(root)
+                folder_e = ET.SubElement(root, 'folder')
+                folder_e.text = folder_name
+                f_name_e = ET.SubElement(root, 'filename')
+                f_name_e.text = im_id + '.tif'
 
-    obj_e = ET.SubElement(root, 'object')
-    name_e = ET.SubElement(obj_e, 'name')
-    name_e.text = 'building'
-    bndbox_e = ET.SubElement(obj_e, 'bndbox')
-
-    poly_pts = []
-    for p in range(pts.GetPointCount()):
-        poly_pts.append([int(round(pts.GetX(p))), int(round(pts.GetY(p)))])
-        print 'x {}, y {}'.format(poly_pts[-1][0], poly_pts[-1][1])
-        # f.write('x {}, y {}\n'.format(poly_pts[-1][0], poly_pts[-1][1]))
-
-    
-    poly_pts = np.array(poly_pts[0:-1], np.int32)
-    poly_pts = poly_pts.reshape((-1,1,2))
-        
-    ok, mnx_mny_mxx_mxy, width, height, area = bounding_rect_min_max(poly_pts)
-    (mnx, mny, mxx, mxy) = mnx_mny_mxx_mxy 
-
-    xmin_e = ET.SubElement(bndbox_e, 'xmin') 
-    xmin_e.text = str(int(mnx))
-    ymin_e = ET.SubElement(bndbox_e, 'ymin') 
-    ymin_e.text = str(int(mny))
-    xmax_e = ET.SubElement(bndbox_e, 'xmax') 
-    xmax_e.text = str(int(mxx))
-    ymax_e = ET.SubElement(bndbox_e, 'ymax') 
-    ymax_e.text = str(int(mxy))
+                # set_trace()
+                im = cv2.imread('/home/tempuser/spacenet-data/3band' + '/' + '3band_' + f_name_e.text)
+                height, width, depth = im.shape # (width,height) tuple
+                size_e = ET.SubElement(root, 'size')
+                depth_e = ET.SubElement(size_e, 'depth')
+                depth_e.text = '3'
+                height_e = ET.SubElement(size_e, 'height')
+                height_e.text = str(height)
+                width_e = ET.SubElement(size_e, 'width')
+                width_e.text = str(width)
 
 
-    print 'box params w:{}, h:{}, area:{}\n'.format(width, height, area)
-    # f.write('box params w:{}, h:{}, area:{}\n'.format(width, height, area))
+            pts = geom.GetGeometryRef(0)
+            print '--------{}----{}------\n'.format(im_id, b_id)
+            # f.write('--------{}----{}------\n'.format(im_id, b_id))
 
-indent(root)
-# tree.write(sys.stdout)
-tree.write(f)
-f.close()
-os.remove(out_dir + '/' + 'empty.xml')
+            obj_e = ET.SubElement(root, 'object')
+            name_e = ET.SubElement(obj_e, 'name')
+            name_e.text = 'building'
+            bndbox_e = ET.SubElement(obj_e, 'bndbox')
+
+            poly_pts = []
+            for p in range(pts.GetPointCount()):
+                poly_pts.append([int(round(pts.GetX(p))), int(round(pts.GetY(p)))])
+                print 'x {}, y {}'.format(poly_pts[-1][0], poly_pts[-1][1])
+                # f.write('x {}, y {}\n'.format(poly_pts[-1][0], poly_pts[-1][1]))
+
+
+            poly_pts = np.array(poly_pts[0:-1], np.int32)
+            poly_pts = poly_pts.reshape((-1,1,2))
+
+            ok, mnx_mny_mxx_mxy, width, height, area = bounding_rect_min_max(poly_pts)
+            (mnx, mny, mxx, mxy) = mnx_mny_mxx_mxy
+
+            xmin_e = ET.SubElement(bndbox_e, 'xmin')
+            xmin_e.text = str(int(mnx))
+            ymin_e = ET.SubElement(bndbox_e, 'ymin')
+            ymin_e.text = str(int(mny))
+            xmax_e = ET.SubElement(bndbox_e, 'xmax')
+            xmax_e.text = str(int(mxx))
+            ymax_e = ET.SubElement(bndbox_e, 'ymax')
+            ymax_e.text = str(int(mxy))
+
+
+            print 'box params w:{}, h:{}, area:{}\n'.format(width, height, area)
+            # f.write('box params w:{}, h:{}, area:{}\n'.format(width, height, area))
+
+        indent(root)
+        # tree.write(sys.stdout)
+        tree.write(f)
+        f.close()
+        os.remove(out_dir + '/' + 'empty.xml')
